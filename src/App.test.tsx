49bea19e3cee2ecaf69aa398f8c79287c7bba85b
Daniel Barreto <munizbarreto5@gmail.com>
2021-11-05 01:18:23 -0300

    chore: add eslint config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,5 +5,6 @@ import App from './App';
 test('renders learn react link', () => {
     render(<App />);
     const linkElement = screen.getByText(/Hello World/i);
+
     expect(linkElement).toBeInTheDocument();
 });
